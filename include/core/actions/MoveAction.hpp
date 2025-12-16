f631c166109fc914830d47cfbb437f2b95f41109
BatsCrow <twilliamsa776@gmail.com>
2025-12-15 23:03:16 -0500

    ADDED: Action Class Interfaces

---
+++ include/core/actions/MoveAction.hpp
@@ -0,0 +1,21 @@
+// include/core/actions/MoveAction.hpp
+
+#pragma once
+
+#include <string>
+#include <filesystem>
+#include "Action.hpp"
+
+
+class MoveAction: public Action {
+    public: 
+        MoveAction(std::filesystem::path destination);
+
+        ActionType type() const override;
+        std::string describe() const override;
+        std::filesystem::path destination() const;
+
+    private:
+        std::filesystem::path destination_;
+
+};
