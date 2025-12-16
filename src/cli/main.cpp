ab5456d4132826c974a96fa7111202e8e3c3e3a1
BatsCrow <twilliamsa776@gmail.com>
2025-12-16 18:12:30 -0500

    UPDATE: moved main.cpp

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,9 +1,16 @@
 #include "folder_sort/scan.hpp"
+#include "core/model/FileInfo.h"
 #include <iostream>
 
 int main(int argc, char** argv) {
     const char* path = (argc > 1) ? argv[1] : ".";
     int rc = folder_sort::scan_directory(path);
+
+    FileInfo ftest;
+    ftest.name = "test.jpg";
+
+    std::cout << ftest.name << std::endl;
+
     if (rc != 0) {
         std::cerr << "scan_directory failed with code " << rc << "\n";
         return rc;
