2be7cb4eeb3e77f1a1ee824ff1a6a2572372a9d5
BatsCrow <twilliamsa776@gmail.com>
2025-12-16 18:25:41 -0500

    FIX: Removed legacy from main

--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,20 +1,6 @@
-#include "folder_sort/scan.hpp"
-#include "core/model/FileInfo.h"
 #include <iostream>
 
 int main(int argc, char** argv) {
-    const char* path = (argc > 1) ? argv[1] : ".";
-    int rc = folder_sort::scan_directory(path);
-
-    FileInfo ftest;
-    ftest.name = "test.jpg";
-
-    std::cout << ftest.name << std::endl;
-
-    if (rc != 0) {
-        std::cerr << "scan_directory failed with code " << rc << "\n";
-        return rc;
-    }
-    std::cout << "folder-sort completed successfully.\n";
+    std::cout << "Build Successful!" << std::endl;
     return 0;
 }
