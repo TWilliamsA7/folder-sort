588a9c535cb4d8eed387aa674608943108262966
BatsCrow <twilliamsa776@gmail.com>
2025-12-19 23:00:44 -0500

    ADDED: ConfigLoader header and implementation

---
+++ include/config/ConfigLoader.hpp
@@ -0,0 +1,19 @@
+// include/config/ConfigLoader.hpp
+
+#pragma once
+
+#include <string>
+#include <stdexcept>
+#include <yaml-cpp/yaml.h>
+
+class ConfigLoader {
+    public:
+        // * Initialize Loader pointed at YAML file at path
+        explicit ConfigLoader(const std::string& path);
+
+        // * Load YAML file located at path_
+        YAML::Node load() const;
+
+    private:
+        std::string path_;
+};
