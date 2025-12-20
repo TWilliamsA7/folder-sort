3167ee01f922ebb0e95e255abd647e42c9cc8e96
BatsCrow <twilliamsa776@gmail.com>
2025-12-20 00:29:28 -0500

    UPDATE: rule apply method

--- include/core/rules/Rule.hpp
+++ include/core/rules/Rule.hpp
@@ -7,9 +7,13 @@
 
 // Defines a set of conditions that will trigger a set of actions
 // Immutable after Creation
+// For actions to be applied, a file must meet all of the expressed conditions
 class Rule {
     public:
         Rule(std::string name, std::vector<std::unique_ptr<Condition>> conditions, std::vector<std::unique_ptr<Action>> actions);
+
+        // * Returns set of actions to apply to a given file depending on conditions
+        std::vector<std::unique_ptr<Action>> apply(const FileInfo&) const;
 
         const std::string name() const;
         const std::vector<std::unique_ptr<Condition>> conditions() const;
--- src/core/rules/Rule.cpp
+++ src/core/rules/Rule.cpp
@@ -5,6 +5,14 @@
 Rule::Rule(std::string name, std::vector<std::unique_ptr<Condition>> conditions, std::vector<std::unique_ptr<Action>> actions
     ) : name_(std::move(name)), conditions_(std::move(conditions)), actions_(std::move(actions)) {}
 
+
+std::vector<std::unique_ptr<Action>> Rule::apply(const FileInfo& file) const {
+    for (const auto& condition : conditions_) {
+        if (!condition.get()->check(file)) return {};
+    }
+    return actions_;
+}
+
 const std::string Rule::name() const { return name_; }
 const std::vector<std::unique_ptr<Condition>> Rule::conditions() const { return conditions_; }
 const std::vector<std::unique_ptr<Action>> Rule::actions() const { return actions_; }
