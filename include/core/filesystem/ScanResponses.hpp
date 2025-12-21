bd202864c9a29945c1bf72ee542e09a880d7b885
BatsCrow <twilliamsa776@gmail.com>
2025-12-21 14:50:08 -0500

    Update: Moved FileInfo

--- include/core/filesystem/ScanResponses.hpp
+++ include/core/filesystem/ScanResponses.hpp
@@ -1,10 +1,21 @@
-// include/core/model/FileInfo.hpp
-
-#pragma once
+// include/core/filesystem/ScanResponses.hpp
 
 #include <filesystem>
 #include <optional>
 #include <string>
+
+enum class ScanErrorType {
+    PermissionDenied,
+    NotFound,
+    SymlinkLoop,
+    Unknown
+};
+
+struct ScanError {
+    std::filesystem::path path;
+    ScanErrorType type;
+    std::error_code ec;
+};
 
 struct FileInfo {
     std::filesystem::path path;
@@ -13,4 +24,9 @@ struct FileInfo {
 
     std::string filename() const { return path.filename().string(); }
     std::string extension() const { return path.extension().string(); }
+};
+
+struct ScanResult {
+    std::vector<FileInfo> files;
+    std::vector<ScanError> errors;
 };
