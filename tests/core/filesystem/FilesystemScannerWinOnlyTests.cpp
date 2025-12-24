c6474e52673e01d49afde1a3836416f900775610
BatsCrow <twilliamsa776@gmail.com>
2025-12-23 19:36:47 -0500

    ADD: platform-specific scanner tests

---
+++ tests/core/filesystem/FilesystemScannerWinOnlyTests.cpp
@@ -0,0 +1,43 @@
+// tests/core/filesystem/FilesystemScannerWinOnlyTests.cpp
+
+#ifdef _WIN32
+
+#include <gtest/gtest.h>
+#include <windows.h>
+#include "../tests/helpers/TestFilesystem.hpp"
+#include "core/filesystem/FilesystemScanner.hpp"
+
+TEST(ScannerWindowsTests, SkipsHiddenFilesByDefault) {
+    TestTree dir("scanner_win_hidden");
+
+    auto hidden = dir.file("hidden.txt");
+
+    SetFileAttributesW(hidden.wstring().c_str(), FILE_ATTRIBUTE_HIDDEN);
+
+    ScanOptions opts;
+
+    FilesystemScanner scanner(dir.root, opts);
+    auto result = scanner.scan();
+
+    EXPECT_TRUE(result.files.empty());
+}
+
+
+TEST(ScannerWindowsTests, IncludesHiddenFilesWhenRequested) {
+    TestTree dir("scanner_win_hidden");
+
+    auto hidden = dir.file("hidden.txt");
+
+    SetFileAttributesW(hidden.wstring().c_str(), FILE_ATTRIBUTE_HIDDEN);
+
+    ScanOptions opts;
+    opts.include_hidden = true;
+
+    FilesystemScanner scanner(dir.root, opts);
+    auto result = scanner.scan();
+
+    EXPECT_EQ(result.files.size(), 1);
+}
+
+
+#endif
